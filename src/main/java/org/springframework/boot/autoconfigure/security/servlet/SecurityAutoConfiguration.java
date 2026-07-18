449d4c265fa1fc9f0ab4028eb3700d077101e119
Apple <apple@Apples-MacBook-Pro.local>
2026-07-18 11:07:45 +0530

    Initial commit

--- src/main/java/org/springframework/boot/autoconfigure/security/servlet/SecurityAutoConfiguration.java
+++ src/main/java/org/springframework/boot/autoconfigure/security/servlet/SecurityAutoConfiguration.java
@@ -1,4 +1,4 @@
-package com.example.hotelbooking_app.SecurityConfig;
+package org.springframework.boot.autoconfigure.security.servlet;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,7 +9,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
+public class SecurityAutoConfiguration {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -17,9 +17,12 @@ public class SecurityConfig {
             .cors(Customizer.withDefaults())
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/api/**").permitAll() // அனைத்து யூசர் எண்ட்பாயிண்ட்களையும் அனுமதிக்கும்
+                .requestMatchers("/api/users/**").permitAll() // அனைத்து யூசர் எண்ட்பாயிண்ட்களையும் அனுமதிக்கும்
                 .anyRequest().authenticated()
             );
         return http.build();
     }
 }
+
+
+
